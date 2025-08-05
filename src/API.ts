e429c9c69abcb3231f3506b28a254c4c4af59391
Pallab Jyoti Bora <pallabjyotibora75@gmail.com>
2025-08-06 01:13:59 +0530

    first commit

---
+++ src/API.ts
@@ -0,0 +1,47 @@
+import { shuffleArray } from "./utils";
+
+export type Question = {
+    category: string;
+    correct_answer: string;
+    difficulty: string;
+    incorrect_answers: string[];
+    question: string;
+    type: string;
+};
+
+export type QuestionState = Question & { answers: string[] };
+
+export enum Difficulty {
+    EASY = 'easy',
+    MEDIUM = 'medium',
+    HARD = 'hard',
+}
+
+export const fetchQuizQuestions = async (
+    amount: number,
+    difficulty: Difficulty
+): Promise<QuestionState[]> => {
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+
+    try {
+        const res = await fetch(endpoint);
+        const data = await res.json();
+
+      
+        if (data.response_code !== 0 || !Array.isArray(data.results)) {
+            console.error("Invalid API response:", data);
+            return [];
+        }
+
+        return data.results.map((question: Question) => ({
+            ...question,
+            answers: shuffleArray([
+                ...question.incorrect_answers,
+                question.correct_answer,
+            ]),
+        }));
+    } catch (error) {
+        console.error("Error fetching quiz questions:", error);
+        return [];
+    }
+};
